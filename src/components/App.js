01db776233b8f29f5b0bd0ccc665857fc70aa0d5
Iris Ji <irisji9969@gmail.com>
2019-10-14 11:07:19 +1100

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ import StreamShow from "../streams/streamShow";
 const App = ()=>{
     return(
         <div className = "ui container">
-            <BrowserRouter basename ={window.location.pathname || ''}>
+            <BrowserRouter basename ={`${window.location.pathname || ''}`}>
                 
                 <div>
                     <Header/>
