fd52aa41ec8638b8814405cc2e1437a0b6c12bb0
Iris Ji <irisji9969@gmail.com>
2019-10-16 10:37:58 +1100

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -13,15 +13,15 @@ import StreamShow from "../streams/streamShow";
 const App = ()=>{
     return(
         <div className = "ui container">
-            <BrowserRouter basename ='/client'>
+            <BrowserRouter>
                 
                 <div>
                     <Header/>
                     <Route path="/" exact component={StreamList}/>
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete}/>
-                    <Route path="/streams/show" exact component={StreamShow}/>
+                    <Route path="/#streams/new" exact component={StreamCreate} />
+                    <Route path="/#streams/edit" exact component={StreamEdit} />
+                    <Route path="/#streams/delete" exact component={StreamDelete}/>
+                    <Route path="/#streams/show" exact component={StreamShow}/>
                 </div>
         
             </BrowserRouter>
