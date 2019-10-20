26d29dd4f29b3bc22c08b04178bf734afad217c8
Iris Ji <irisji9969@gmail.com>
2019-10-20 17:01:56 +1100

    no message

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route } from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 
 import Header from "./Header";
 import StreamCreate from "../streams/streamCreate";
@@ -13,18 +13,18 @@ import StreamShow from "../streams/streamShow";
 const App = ()=>{
     return(
         <div className = "ui container">
-            <BrowserRouter basename={'/client'}>
+            <HashRouter basename={'/client'}>
                 
                 <div>
                     <Header/>
-                    <Route path="/" exact component={StreamList}/>
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete}/>
-                    <Route path="/streams/show" exact component={StreamShow}/>
+                        <Route exact path="/" component={StreamList}/>
+                        <Route exact path="/streams/new" component={StreamCreate} />
+                        <Route exact path="/streams/edit" component={StreamEdit} />
+                        <Route exact path="/streams/delete" component={StreamDelete}/>
+                        <Route exact path="/streams/show" component={StreamShow}/>
                 </div>
         
-            </BrowserRouter>
+            </HashRouter>
         </div>
         )
 }
